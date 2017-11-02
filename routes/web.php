322e057cca7ca0d728bd7b28f88ed3851cdbab53
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2017-11-02 09:59:11 +0800

    Create authentication routes

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::view('/', 'welcome')->name('welcome');
 // Registration routes
 Route::get('register', ['as' => 'register', 'uses' => 'Auth\RegisterController@showRegistrationForm']);
 Route::post('register', ['as' => 'register.submit', 'uses' => 'Auth\RegisterController@register']);
+
+// Authentication routes
+Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+Route::post('login', ['as' => 'login.validate', 'uses' => 'Auth\LoginController@login']);
+Route::post('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
