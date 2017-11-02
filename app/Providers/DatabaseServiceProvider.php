0fa4b9f2283d606ab79314663fae8e4ee93a9302
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2017-11-02 09:30:32 +0800

    Create database service provider

---
+++ app/Providers/DatabaseServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DatabaseServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+    }
+}
