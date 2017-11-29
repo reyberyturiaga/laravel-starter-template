334bc26134e5a40c2de90d4152274679879b4f3a
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2017-11-29 08:48:55 +0800

    Remove reference to example vue component in app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,8 +12,6 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
-
 const app = new Vue({
     el: '#app'
 });
