5bf4ea12c4ddd68474fc452ddb681870bd068228
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2017-11-29 09:19:52 +0800

    Require components.js file in app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -32,6 +32,8 @@ if (token) {
 
 window.Vue = require('vue');
 
+require('./components');
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
