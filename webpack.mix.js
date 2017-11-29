638ca558eb7a96ee00b0523ca9190e4035eda898
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2017-11-29 09:28:23 +0800

    Setup extraction of vendor scripts in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
+   .extract(['lodash', 'axios', 'vue'])
    .sass('resources/assets/sass/app.scss', 'public/css');
