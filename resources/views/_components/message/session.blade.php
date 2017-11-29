aeb4090dac80738331f15e0ff5e36e3c93f55a05
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2017-11-29 10:37:15 +0800

    Create session message component

---
+++ resources/views/_components/message/session.blade.php
@@ -0,0 +1,23 @@
+@if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+@endif
+
+@if (session('info'))
+    <div class="alert alert-info">
+        {{ session('info') }}
+    </div>
+@endif
+
+@if (session('warning'))
+    <div class="alert alert-warning">
+        {{ session('warning') }}
+    </div>
+@endif
+
+@if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
