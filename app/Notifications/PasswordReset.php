2e531c2f69e7abfb07867ecd263966b70fc475de
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2017-12-11 11:56:48 +0800

    Create password reset notification

---
+++ app/Notifications/PasswordReset.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordReset extends Notification
+{
+    use Queueable;
+
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    private $token;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param string $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable): MailMessage
+    {
+        return (new MailMessage())
+            ->line('You are receiving this email because we received a password reset request for your account.')
+            ->action('Reset Password', route('password.reset', $this->token))
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function toArray($notifiable): array
+    {
+        return [
+        ];
+    }
+}
