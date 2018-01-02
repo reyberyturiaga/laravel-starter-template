bce94b4369c045bb68829db1341ba940b39523e4
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2018-01-02 15:04:02 +0800

    Create email confirmation notification

---
+++ app/Notifications/EmailConfirmation.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailConfirmation extends Notification
+{
+    use Queueable;
+
+    /**
+     * User activation token.
+     *
+     * @var string
+     */
+    private $token;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param string $token
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function via($notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage())
+            ->subject('Activate Your Account')
+            ->line("You're almost done! Click button below to verify your email address.")
+            ->action('Verify', route('user.activate', $this->token));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     *
+     * @return array
+     */
+    public function toArray($notifiable): array
+    {
+        return [
+        ];
+    }
+}
