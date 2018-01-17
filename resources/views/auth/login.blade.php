2dd5bd6498ac57c00d38e359106e076e49cdbcfc
Reyber Ian E. Yturiaga <reyberyturiaga@gmail.com>
2018-01-17 11:33:19 +0800

    Add social authentication links in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,6 +60,15 @@
                                 </a>
                             </div>
                         </div>
+                        <hr>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <a href="{{ route('login.provider', ['provider' => 'facebook']) }}" class="btn btn-default"><i class="fa fa-facebook"></i> Facebook</a>
+                                <a href="{{ route('login.provider', ['provider' => 'twitter']) }}" class="btn btn-default"><i class="fa fa-twitter"></i> Twitter</a>
+                                <a href="{{ route('login.provider', ['provider' => 'github']) }}" class="btn btn-default"><i class="fa fa-github"></i> Github</a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
